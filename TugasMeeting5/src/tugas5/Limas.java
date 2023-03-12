392f3364c65ae75c93978f3df58ad1a48283fd6b
masaqil55 <111986181+masaqil55@users.noreply.github.com>
2023-03-12 11:23:49 +0700

    Add files via upload

--- TugasMeeting5/src/tugas5/Limas.java
+++ TugasMeeting5/src/tugas5/Limas.java
@@ -4,10 +4,67 @@
  */
 package tugas5;
 
-/**
- *
- * @author aqil
- */
 public class Limas {
-    
+    private float panjangAlas;
+    private float lebarAlas;
+    private float tinggi;
+    private float tinggiLimas;
+
+    public Limas (float panjangAlas, float lebarAlas, float tinggi, float tinggiLimas) {
+        this.panjangAlas = panjangAlas;
+        this.lebarAlas = lebarAlas;
+        this.tinggi = tinggi;
+        this.tinggiLimas = tinggiLimas;
+    }
+
+    public Limas (float panjangAlas, float lebarAlas, float tinggi) {
+        this.panjangAlas = panjangAlas;
+        this.lebarAlas = lebarAlas;
+        this.tinggi = tinggi;
+        this.tinggiLimas = tinggi / 2;
+    }
+
+    public Limas (float panjangAlas, float tinggi) {
+        this.panjangAlas = panjangAlas;
+        this.lebarAlas = panjangAlas;
+        this.tinggi = tinggi;
+        this.tinggiLimas = tinggi / 2;
+    }
+
+    public float volumeResult () {
+        float volume = (panjangAlas * lebarAlas * tinggi)/3;
+        return volume;
+    }
+
+    public float getPanjangAlas () {
+        return panjangAlas;
+    }
+
+    public void setPanjangAlas (float panjangAlas) {
+        this.panjangAlas = panjangAlas;
+    }
+
+    public float getLebarAlas () {
+        return lebarAlas;
+    }
+
+    public void setLebarAlas (float lebarAlas) {
+        this.lebarAlas = lebarAlas;
+    }
+
+    public float getTinggi () {
+        return tinggi;
+    }
+
+    public void setTinggi (float tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    public float getTinggiLimas () {
+        return tinggiLimas;
+    }
+
+    public void setTinggiLimas (float tinggiLimas) {
+        this.tinggiLimas = tinggiLimas;
+    }
 }
